41520aaff9242e8254b5740abbf2e5655c3d6e95
Shivam Sharma <shivam14052004@gmail.com>
2025-11-03 06:45:42 +0530

    Created initial Schema

---
+++ backend/models/user.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true
+    },email: {
+        type: String,
+        required: true,
+        unique: true
+    },password: {
+        type: String,
+        //If someone prefer to login through google auth, for that reason we are not making the required as true.
+    },mobileNo: {
+        type: String,
+        required: true
+    },role: {
+        type: String,
+        enum: ["user", "owner", "deliveryPartner"],
+        required: true
+    }              
+}, {
+   timestamps: true
+})
+
+const User = mongoose.model('User', userSchema)
+
+export default User
